17dd5be2a28b20750f5861d5fe0373a8b456c0b3
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-03-26 19:09:35 -0300

    feat: mock endpoint
    Mock root endpoint for test only.

--- src/server.js
+++ src/server.js
@@ -3,4 +3,8 @@ import Express from 'express';
 const app = Express();
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('HELLO WORLD !!!');
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
