da9d70d09107423732bda45ebbc140ce05c35f76
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-03-31 21:41:14 -0300

    feat: pages folder
    Bootstraps a page components setup and creates the home page.

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+// External
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({});
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <main>
+      <h1>Página Principal</h1>
+    </main>
+  )
+};
+
+export default Home;
