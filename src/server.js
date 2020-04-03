5cb352ecc7c4e5134dc394f7a8539445564f6034
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-04-03 20:41:32 -0300

    feat: graphql and apollo server
    Adds GraphQL and Apollo server for express, and do a basic setup
    in the server file.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,35 @@
-import Express from 'express';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
 
-const app = Express();
-const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.send('HELLO WORLD !!!');
-})
+const schema = gql`
+  type Query {
+    me: User
+  }
+  type User {
+    username: String!
+  }
+`;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Eric Madureira Argolo',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen(port, () => console.log(`Apollo Server on http://localhost:${port}/graphql`));
